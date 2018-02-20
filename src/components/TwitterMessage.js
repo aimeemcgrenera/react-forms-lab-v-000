646471c558f1a3381e97dcae5efb063f7443dfbf
Aimee McGrenera <aimee.mcgrenera@gmail.com>
2018-02-20 20:40:16 +0000

    automatically backed up by learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -11,7 +11,7 @@ class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" 
+        <input
         type="text" 
         value={this.state.value} 
         onChange={this.handleChange}
